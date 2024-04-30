a9a2d79fa61ba1b08340263b1618449a7fbf0344
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2024-04-30 12:37:47 +0300

    Add cli

---
+++ cli/cli.go
@@ -0,0 +1,16 @@
+package cli
+
+import (
+	"flag"
+)
+
+var (
+	ComposeFilePath 	string
+	IniFilePath 		string
+)
+
+func ArgsInit() {
+	flag.StringVar(&ComposeFilePath,"c", "docker-compose.yml", "Path to Compose file")
+	flag.StringVar(&IniFilePath,"i", "result.service", "Path to resulting service file")
+	flag.Parse()
+}
--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"github.com/nevermarine/gloco/parse"
+	"github.com/nevermarine/gloco/cli"
 	"fmt"
 )
 
 func main() {
-	project, err := parse.LoadFile("docker-compose.yml")
+	cli.ArgsInit()
+
+	project, err := parse.LoadFile(cli.ComposeFilePath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = parse.WriteIni(project, "postgres.service")
+	err = parse.WriteIni(project, cli.IniFilePath)
 	if err != nil {
 		fmt.Println(err)
 		return
